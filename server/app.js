036bc6d46d5e84569e690db2588f1e84ed89b787
Peanuts-83 <tranque@free.fr>
2022-08-03 14:57:41 +0200

    change path

--- server/app.js
+++ server/app.js
@@ -11,12 +11,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 module.exports = app;
